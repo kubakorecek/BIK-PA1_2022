ba5598e3aebf5eeb50b63f1bd17cc75c923d28fe
Jakub Koreček <korecjak@fit.cvut.cz>
2022-10-26 20:52:08 +0200

    hw02: naive trial works

--- hw02/main.c
+++ hw02/main.c
@@ -10,7 +10,6 @@
 #include <assert.h>
 #endif
 
-
 /** @brief Print error message
  *  @param inputCode Number, which can help point where error happened
  *  @return Void.
@@ -25,125 +24,138 @@ void errorMessage(int const inputCode)
     printf("Nespravny vstup. at %d \n", inputCode);
 }
 
-int sumDivisors( int divisors[MAX_FACOTRS], int * n )
+int sumDivisors(int divisors[MAX_FACOTRS], int *n)
 {
     int sum = 1;
-    for(int j = 1 ; j < (*n); j++ )
+    for (int j = 1; j < (*n); j++)
     {
         sum += divisors[j];
     }
     return sum;
 }
 
-int isPrimeTrial( int number)
+int isPrimeTrial(int number)
 {
-    if( (number ==2) || (number == 3) ||(number == 1)){return TRUE;}
+    if ((number == 2) || (number == 3) || (number == 1))
+    {
+        return TRUE;
+    }
 
-    int upBound = trunc(sqrt( number ));
-    int sum = 1;
+    int upBound = trunc(sqrt(number));
     for (int i = 2; i <= upBound; i++)
     {
-        if((number % i ) == 0)
-        {return FALSE;}
+        if ((number % i) == 0)
+        {
+            return FALSE;
+        }
     }
-   // printf("is PRIME %d\n",number);
+    // printf("is PRIME %d\n",number);
     return TRUE;
 }
 
-
-void getDivisors(const int number, int * n, int flag)
+void getDivisors(const int number, int *n, int flag)
 {
-    int upBound = trunc(sqrt( number ));
+    int upBound = trunc(sqrt(number));
     int sum = 1;
     int divisors = 0;
     for (int i = 2; i <= upBound; i++)
     {
-        if(number%i == 0)
-        {   
+        if (number % i == 0)
+        {
             divisors++;
-            if((number / i) == i)
+            if ((number / i) == i)
             {
-                sum +=i;
-                //printf("i %d\n", i);
-            }else 
+                sum += i;
+                // printf("i %d\n", i);
+            }
+            else
             {
-                //printf("i %d i %d\n", i, (number / i));
-                sum +=i;
-                sum +=(number / i);
+                // printf("i %d i %d\n", i, (number / i));
+                sum += i;
+                sum += (number / i);
             }
         }
-        
     }
-    if(sum == 1)
+    if (sum == 1)
     {
         return;
     }
-    if(isPrimeTrial(sum) ==TRUE)
+    if (isPrimeTrial(sum) == TRUE)
     {
         (*n)++;
-        if(flag==TRUE)
+        if (flag == TRUE)
         {
-            printf("%d\n",number);
+            printf("%d\n", number);
         }
         // printf("is PRIME n %d %d %d\n", (*n), sum, number);
     }
-    //printf("divisors:%d sum: %d\n", divisors, sum);
-
+    // printf("divisors:%d sum: %d\n", divisors, sum);
 }
 
 #ifndef DEBUG
-int main( void )
+int main(void)
 {
 
-    int hi = 0; int low = 0; char c;
-    int check = EOF; int n = 0;
+    int hi = 0;
+    int low = 0;
+    int flag = FALSE;
+    char c = ' ';
+    int check = EOF;
+    int n = 0;
     printf("Intervaly:\n");
-    while( (check = scanf("%c %d %d")) == 3 && check !=EOF)
+    while ((check = scanf(" %c %d %d ", &c, &low, &hi)) == 3 && check != EOF)
     {
-        if( !(c == '#' || c == '?' ))
+        if (!(c == '#' || c == '?'))
         {
             errorMessage(0);
-        }else if( low > hi) 
-        {   
-            errorMessage(0);
+            return 0;
         }
-        else 
+        else if ((low > hi) || check !=3 || low < 1 || hi < 1)
         {
-                n = 0;
-    for(int s = 100; s <= 120; s++)
-    {
-        getDivisors(s,&n);
-    }
-      printf("Celkem:%d\n", n);
+            errorMessage(0);
+            return 0;        }
+        else
+        {
+            n = 0;
+            flag = FALSE;
+            if( c == '?')
+            {
+                flag = TRUE;    
+            }
+            for (int s = low; s <= hi; s++)
+            {
+                getDivisors(s, &n, flag);
+            }
+            printf("Celkem: %d\n", n);
         }
-        
     }
-
-
-
+    if ( check != EOF)
+    {
+      errorMessage(0);  
+    }
     return 0;
 }
-#else 
-int main( void )
+#else
+int main(void)
 {
     printf("debug mode\n");
     int n = 0;
-    getDivisors(27,&n, TRUE);
+    getDivisors(27, &n, TRUE);
     printf("n: %d\n", n);
     n = 0;
-    for(int s = 100; s <= 120; s++)
+    for (int s = 100; s <= 120; s++)
     {
-        getDivisors(s,&n, TRUE);
+        getDivisors(s, &n, TRUE);
     }
-      printf("n: %d\n", n);
-      assert(2 == n);
+    printf("n: %d\n", n);
+    assert(2 == n);
     n = 0;
-    for(int s = 265; s <= 327; s++)
+    for (int s = 265; s <= 327; s++)
     {
-        getDivisors(s,&n, TRUE);
+        getDivisors(s, &n, TRUE);
     }
-      printf("n: %d\n", n);
-      assert(12 == n);
+    printf("n: %d\n", n);
+    assert(12 == n);
     return 0;
 }
 
